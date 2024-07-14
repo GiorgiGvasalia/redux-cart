aad075d97e5cd4c0dcb9ed5b1ccb93ff2b80d609
GiorgiGvasalia <ggvasalia4@gmail.com>
2024-07-14 16:56:39 +0400

    adding products to a cart && displaying quantity & price etc.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,20 +1,22 @@
 import classes from './CartButton.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
 
   const dispatch = useDispatch()
+  const cartQuantity = useSelector(state => state.cart.totalQuantity)
 
   const cartToggleHnadle = () => {
     dispatch(uiActions.toggle())
   }
 
 
+
   return (
     <button className={classes.button} onClick={cartToggleHnadle}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity} items</span>
     </button>
   );
 };
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,6 +1,7 @@
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
+  
   const { title, quantity, total, price } = props.item;
 
   return (
